ddcea763edcd06692f38de761e41291d0dc3e060
Said Hazem <1saidhazem@gmail.com>
2022-05-11 21:49:11 +0300

    bush

--- app/src/main/java/com/example/drrbni/ui/ProfileFragment.java
+++ app/src/main/java/com/example/drrbni/ui/ProfileFragment.java
@@ -5,13 +5,17 @@ import static com.example.drrbni.Constant.EMAIL;
 import static com.example.drrbni.Constant.NAME;
 import static com.example.drrbni.Constant.SPECIALIZATION;
 import static com.example.drrbni.Constant.UNIVERSITY;
+
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.drrbni.Models.Student;
 import com.example.drrbni.databinding.FragmentProfileBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -77,10 +81,10 @@ public class ProfileFragment extends Fragment {
 
     private Student getInfoProfile() {
         fireStore.collection(COLLECTION_STUDENT_PROFILES)
-                .whereEqualTo(EMAIL,currentUser.getEmail())
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                .document(currentUser.getUid())
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
                     @Override
-                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
                         if (error != null) {
                             Log.w("ttt", "error" + error);
                             return;
@@ -88,22 +92,24 @@ public class ProfileFragment extends Fragment {
 
                         student = new Student();
                         if (value != null) {
-                            for (DocumentSnapshot doc : value) {
-                                    student.setEmail(doc.getString(EMAIL));
-                                    student.setName(doc.getString(NAME));
-                                    student.setUniversity(doc.getString(UNIVERSITY));
-                                    student.setSpecialization(doc.getString(SPECIALIZATION));
-                            }
-                            Log.d("ttt", "email: "+student.getEmail());
-                            Log.d("ttt", "Specialization: "+student.getSpecialization());
-                            Log.d("ttt", "University: "+student.getUniversity());
-                        }else {
+                            student.setEmail(value.getString(EMAIL));
+                            student.setName(value.getString(NAME));
+                            student.setUniversity(value.getString(UNIVERSITY));
+                            student.setSpecialization(value.getString(SPECIALIZATION));
+
+                            Log.d("ttt", "email: " + value.getString(EMAIL));
+                            Log.d("ttt", "Specialization: " + student.getSpecialization());
+                            Log.d("ttt", "University: " + student.getUniversity());
+                        } else {
                             Log.e("ttt", "value is null");
                         }
-
                     }
                 });
         return student;
     }
+
+    /*
+
+     */
 
 }
